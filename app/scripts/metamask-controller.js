70042280d1d6f35ae109ed9a8ca9bdc34a6390ec
Jongsun Suh <jongsun.suh@icloud.com>
2024-12-02 03:13:08 -0500

    Remove `NotificationController` from `MetamaskController` and store types

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2554,7 +2554,6 @@ export default class MetamaskController extends EventEmitter {
       SnapController: this.snapController.state,
       CronjobController: this.cronjobController.state,
       SnapsRegistry: this.snapsRegistry.state,
-      NotificationController: this.notificationController.state,
       SnapInterfaceController: this.snapInterfaceController.state,
       SnapInsightsController: this.snapInsightsController.state,
       ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
@@ -2611,7 +2610,6 @@ export default class MetamaskController extends EventEmitter {
       SnapController: this.snapController.state,
       CronjobController: this.cronjobController.state,
       SnapsRegistry: this.snapsRegistry,
-      NotificationController: this.notificationController.state,
       SnapInterfaceController: this.snapInterfaceController.state,
       SnapInsightsController: this.snapInsightsController.state,
       ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
