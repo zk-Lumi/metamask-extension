3646fe283e2982cfeeef2dc992e17d22ffcc7fab
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-12-09 09:53:29 +0100

    Add support for new Snaps state methods

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -6113,6 +6113,10 @@ export default class MetamaskController extends EventEmitter {
 
     engine.push(
       createSnapsMethodMiddleware(subjectType === SubjectType.Snap, {
+        clearSnapState: this.controllerMessenger.call.bind(
+          this.controllerMessenger,
+          'SnapController:clearSnapState',
+        ),
         getUnlockPromise: this.appStateController.getUnlockPromise.bind(
           this.appStateController,
         ),
@@ -6134,6 +6138,14 @@ export default class MetamaskController extends EventEmitter {
           this.controllerMessenger,
           'SnapController:getFile',
           origin,
+        ),
+        getSnapState: this.controllerMessenger.call.bind(
+          this.controllerMessenger,
+          'SnapController:getSnapState',
+        ),
+        updateSnapState: this.controllerMessenger.call.bind(
+          this.controllerMessenger,
+          'SnapController:updateSnapState',
         ),
         installSnaps: this.controllerMessenger.call.bind(
           this.controllerMessenger,
