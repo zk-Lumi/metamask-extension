74b79069e06df1174e3a6eeddf5db6ef7800e793
dddddanica <zhaodanica@gmail.com>
2024-12-11 00:45:20 +0000

    feat(3744): Implement feature Flag Values with Scope Based on threshold

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2399,6 +2399,9 @@ export default class MetamaskController extends EventEmitter {
         allowedEvents: [],
       }),
       disabled: !this.preferencesController.state.useExternalServices,
+      getMetaMetricsId: Promise.resolve(
+        this.metaMetricsController.getMetaMetricsId(),
+      ),
       clientConfigApiService: new ClientConfigApiService({
         fetch: globalThis.fetch.bind(globalThis),
         config: {
