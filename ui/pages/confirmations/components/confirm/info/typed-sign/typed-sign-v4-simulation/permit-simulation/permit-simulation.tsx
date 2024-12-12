36bd22dc6c9a8f67e0e461bf8f9554272566990c
cryptotavares <joao.tavares@consensys.net>
2024-12-12 15:51:11 +0000

    fix: unused prop

--- ui/pages/confirmations/components/confirm/info/typed-sign/typed-sign-v4-simulation/permit-simulation/permit-simulation.tsx
+++ ui/pages/confirmations/components/confirm/info/typed-sign/typed-sign-v4-simulation/permit-simulation/permit-simulation.tsx
@@ -87,7 +87,6 @@ const PermitSimulation: React.FC<object> = () => {
                 <TokenDetail
                   token={token}
                   amount={amount}
-                  i={i}
                   key={`${token}-${i}`}
                 />
               ),
